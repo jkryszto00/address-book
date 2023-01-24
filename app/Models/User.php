7b4550eafe5d531bd215e626c00a42a00557dc2e
jkryszto00 <jakubkryszto@gmail.com>
2023-01-24 02:04:57 +0100

    Laratrust

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use LaratrustUserTrait, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
