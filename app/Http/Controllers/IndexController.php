8d8c88d0bc379a35b74f0e115191d198f700a4a9
jkryszto00 <jakubkryszto@gmail.com>
2023-01-24 23:18:04 +0100

    Index api endpoint

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,12 +6,21 @@ use App\Http\Resources\AddressResource;
 use App\Models\Address;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Spatie\Searchable\Search;
 
 class IndexController extends Controller
 {
     public function __invoke(Request $request): JsonResponse
     {
-        $addresses = Address::filter($request->only('number', 'street', 'city', 'zip'))->inRandomOrder()->limit(10)->get();
+        $q = $request->get('q');
+
+        if ($q) {
+            $addresses = (new Search())->registerModel(Address::class, ['number', 'street', 'city', 'zip'])
+                ->search($q)
+                ->map(fn ($searched) => $searched->searchable);
+        } else {
+            $addresses = Address::inRandomOrder()->limit(10)->get();
+        }
 
         return response()->json(AddressResource::collection($addresses));
     }
