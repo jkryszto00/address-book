d7c373685ad137f3e3a0a4c045f4506afa7763bb
jkryszto00 <jakubkryszto@gmail.com>
2023-01-24 01:52:58 +0100

    SPA address crud routes

---
+++ resources/js/pages/Address/Edit.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref, defineProps, onMounted } from "vue";
+import { NCard, NButton, NForm, NFormItem, NInput, NGrid, NGi } from 'naive-ui'
+import useAddresses from "../../composables/addresses"
+
+const { address, updateAddress, getAddress } = useAddresses()
+
+const props = defineProps({
+    id: {
+        required: true,
+        type: String
+    }
+})
+const formRef = ref(null)
+
+const rules = {
+    number: {
+        required: true,
+        message: 'Street number is required'
+    },
+    street: {
+        required: true,
+        message: 'Street number is required'
+    },
+    city: {
+        required: true,
+        message: 'City number is required'
+    },
+    zip: {
+        required: true,
+        message: 'Zip number is required'
+    }
+}
+
+onMounted(() => getAddress(props.id))
+
+const addAddress = async (e) => {
+    formRef.value?.validate(async (errors) => {
+        if (!errors) {
+            await updateAddress(props.id)
+        }
+    })
+}
+</script>
+<template>
+    <n-card title="Add address">
+        <n-form
+            ref="formRef"
+            :model="address"
+            :rules="rules"
+        >
+            <n-grid x-gap="12" cols="2">
+                <n-gi>
+                    <n-form-item label="Number" path="number">
+                        <n-input v-model:value="address.number" />
+                    </n-form-item>
+                </n-gi>
+                <n-gi>
+                    <n-form-item label="Street" path="street">
+                        <n-input v-model:value="address.street" />
+                    </n-form-item>
+                </n-gi>
+            </n-grid>
+            <n-grid x-gap="12" cols="2">
+                <n-gi>
+                    <n-form-item label="City" path="city">
+                        <n-input v-model:value="address.city" />
+                    </n-form-item>
+                </n-gi>
+                <n-gi>
+                    <n-form-item label="Zip Code" path="zip">
+                        <n-input v-model:value="address.zip" />
+                    </n-form-item>
+                </n-gi>
+            </n-grid>
+        </n-form>
+        <template #action>
+            <n-button type="info" :block="true" @click="addAddress">Update address</n-button>
+        </template>
+    </n-card>
+</template>
--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { onMounted } from 'vue'
+import { NCard, NList, NListItem, NThing } from 'naive-ui'
+
+import useAddresses from "../composables/addresses"
+
+const { addresses, getAddresses } = useAddresses()
+
+onMounted(getAddresses)
+</script>
 <template>
-    <h1>Index</h1>
+    <n-card title="Addresses">
+        <n-list>
+            <template v-for="address in addresses" :key="address.id">
+                <n-list-item>
+                    <n-thing
+                        :title="`${address.number} ${address.street}`"
+                        :description="`${address.city}, ${address.zip}`"
+                    />
+                </n-list-item>
+            </template>
+        </n-list>
+    </n-card>
 </template>
