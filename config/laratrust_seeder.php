162dce48583adaf1099fcac168ecc48c856477b3
jkryszto00 <jakubkryszto@gmail.com>
2023-01-24 02:13:43 +0100

    Laratrust seeder

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => true,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'superadministrator' => [
+            'addresses' => 'c,r,u,d',
+            'users' => 'c,r,u,d',
+        ],
+        'administrator' => [
+            'addresses' => 'c,r,u,d',
+            'users' => 'r,u'
+        ],
+        'user' => [
+            'addresses' => 'r,u',
+        ]
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
