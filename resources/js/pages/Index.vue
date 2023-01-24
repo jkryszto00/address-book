64f0f3a6361eae59c6ea460995ff1d6e3505d06e
jkryszto00 <jakubkryszto@gmail.com>
2023-01-24 21:02:52 +0100

    Index spa page

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,15 +1,38 @@
 <script setup>
-import { onMounted } from 'vue'
-import { NCard, NList, NListItem, NThing } from 'naive-ui'
+import { ref, onMounted, watch } from 'vue'
+import { NAutoComplete, NCard, NList, NListItem, NThing } from 'naive-ui'
 
 import useAddresses from "../composables/addresses"
+import usePlacematic from "../3rd/placematic"
+import {isEmpty} from "lodash";
 
-const { addresses, getAddresses } = useAddresses()
+const { addresses, filterAddresses } = useAddresses()
+const { addressSuggestions, getAddressSuggestions } = usePlacematic()
 
-onMounted(getAddresses)
+const search = ref('')
+
+const searchAddress = async (e) => filterAddresses(e)
+
+onMounted(async () => await filterAddresses(null))
+
+watch(search, (newSearch) => {
+    getAddressSuggestions(newSearch)
+
+    if (isEmpty(newSearch)) {
+        filterAddresses(null)
+    }
+})
 </script>
 <template>
     <n-card title="Addresses">
+        <template #header>
+            <n-auto-complete
+                v-model:value="search"
+                :clearable="true"
+                @select="searchAddress"
+                :options="addressSuggestions"
+            />
+        </template>
         <n-list>
             <template v-for="address in addresses" :key="address.id">
                 <n-list-item>
