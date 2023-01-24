33b0ca2d26340722d778d1519749ff7e22e0fff0
jkryszto00 <jakubkryszto@gmail.com>
2023-01-24 01:53:12 +0100

    Routes in spa

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,9 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 
+const Index = () => import('@/pages/Index.vue');
+
 /* Guest */
 const Login = () => import('@/pages/Auth/Login.vue');
 const Register = () => import('@/pages/Auth/Register.vue');
-const Index = () => import('@/pages/Index.vue');
+
+/* Address */
+const AddressMyList = () => import('@/pages/Address/List.vue');
+const AddressAllList = () => import('@/pages/Admin/Address/List.vue');
+const AddressAdd = () => import('@/pages/Address/Add.vue');
+const AddressEdit = () => import('@/pages/Address/Edit.vue');
+
 /* Layout */
 const DefaultLayout = () => import('@/layouts/Default.vue')
 
@@ -26,6 +34,27 @@ const routes = [
                 name: 'Index',
                 path: '/',
                 component: Index,
+            },
+            {
+                name: 'AddressMyList',
+                path: '/address/my',
+                component: AddressMyList
+            },
+            {
+                name: 'AddressAllList',
+                path: '/address/all',
+                component: AddressAllList
+            },
+            {
+                name: 'AddressAdd',
+                path: '/address/add',
+                component: AddressAdd
+            },
+            {
+                name: 'AddressEdit',
+                path: '/address/:id/edit',
+                component: AddressEdit,
+                props: true
             }
         ]
     }
