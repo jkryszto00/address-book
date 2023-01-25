076b5a88b3a0dc9b8a31d2b817dac6ad56466fe5
jkryszto00 <jakubkryszto@gmail.com>
2023-01-25 11:10:05 +0100

    Address crud testings

--- tests/Feature/AddressTest.php
+++ tests/Feature/AddressTest.php
@@ -27,15 +27,15 @@ class AddressTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_address_create()
+    public function test_address_create_as_user()
     {
         $user = User::find(3);
 
         $data = [
-            'number' => '4',
-            'street' => 'Nowa',
-            'city' => 'Przyłęki',
-            'zip' => '86-005'
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
         ];
 
         $response = $this->actingAs($user)->post('/api/addresses', $data);
@@ -48,15 +48,36 @@ class AddressTest extends TestCase
         ]);
     }
 
-    public function test_address_show()
+    public function test_address_create_as_administrator()
+    {
+        $user = User::find(2);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $response = $this->actingAs($user)->post('/api/addresses', $data);
+        $response->assertJsonStructure([
+            'id',
+            'number',
+            'street',
+            'city',
+            'zip'
+        ]);
+    }
+
+    public function test_address_show_own_as_user()
     {
         $user = User::find(3);
 
         $data = [
-            'number' => '4',
-            'street' => 'Nowa',
-            'city' => 'Przyłęki',
-            'zip' => '86-005'
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
         ];
 
         $address = $this->actingAs($user)->post('/api/addresses', $data);
@@ -71,15 +92,80 @@ class AddressTest extends TestCase
         ]);
     }
 
-    public function test_address_update()
+    public function test_address_show_own_as_administrator()
+    {
+        $user = User::find(2);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($user)->post('/api/addresses', $data);
+
+        $response = $this->actingAs($user)->get('/api/addresses/'.$address['id']);
+        $response->assertJsonStructure([
+            'id',
+            'number',
+            'street',
+            'city',
+            'zip'
+        ]);
+    }
+
+    public function test_address_show_not_own_as_user()
+    {
+        $owner = User::find(2);
+        $user = User::find(3);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($owner)->post('/api/addresses', $data);
+
+        $response = $this->actingAs($user)->get('/api/addresses/'.$address['id']);
+        $response->assertStatus(403);
+    }
+
+    public function test_address_show_not_own_as_administrator()
+    {
+        $owner = User::find(3);
+        $user = User::find(2);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($owner)->post('/api/addresses', $data);
+
+        $response = $this->actingAs($user)->get('/api/addresses/'.$address['id']);
+        $response->assertJsonStructure([
+            'id',
+            'number',
+            'street',
+            'city',
+            'zip'
+        ]);
+    }
+
+    public function test_address_update_own_as_user()
     {
         $user = User::find(3);
 
         $data = [
-            'number' => '4',
-            'street' => 'Nowa',
-            'city' => 'Przyłęki',
-            'zip' => '86-005'
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
         ];
 
         $address = $this->actingAs($user)->post('/api/addresses', $data);
@@ -94,15 +180,130 @@ class AddressTest extends TestCase
         ]);
     }
 
-    public function test_address_delete()
+    public function test_address_update_own_as_administrator()
+    {
+        $user = User::find(2);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($user)->post('/api/addresses', $data);
+
+        $response = $this->actingAs($user)->put('/api/addresses/'.$address['id'], $data);
+        $response->assertJsonStructure([
+            'id',
+            'number',
+            'street',
+            'city',
+            'zip'
+        ]);
+    }
+
+    public function test_address_update_not_own_as_user()
+    {
+        $own = User::find(2);
+        $user = User::find(3);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($own)->post('/api/addresses', $data);
+
+        $response = $this->actingAs($user)->put('/api/addresses/'.$address['id'], $data);
+        $response->assertStatus(403);
+    }
+
+    public function test_address_update_not_own_as_administrator()
+    {
+        $own = User::find(3);
+        $user = User::find(2);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($own)->post('/api/addresses', $data);
+
+        $response = $this->actingAs($user)->put('/api/addresses/'.$address['id'], $data);
+        $response->assertJsonStructure([
+            'id',
+            'number',
+            'street',
+            'city',
+            'zip'
+        ]);
+    }
+
+    public function test_address_delete_not_own_as_user()
+    {
+        $owner = User::find(2);
+        $user = User::find(3);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($owner)->post('/api/addresses', $data);
+        $response = $this->actingAs($user)->delete('/api/addresses/'.$address['id']);
+        $response->assertStatus(403);
+    }
+
+    public function test_address_delete_not_own_as_administrator()
+    {
+        $owner = User::find(3);
+        $user = User::find(2);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($owner)->post('/api/addresses', $data);
+        $response = $this->actingAs($user)->delete('/api/addresses/'.$address['id']);
+        $response->assertNoContent();
+    }
+
+    public function test_address_delete_own_as_administrator()
+    {
+        $user = User::find(2);
+
+        $data = [
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
+        ];
+
+        $address = $this->actingAs($user)->post('/api/addresses', $data);
+        $response = $this->actingAs($user)->delete('/api/addresses/'.$address['id']);
+        $response->assertNoContent();
+    }
+
+    public function test_address_delete_own_as_user()
     {
         $user = User::find(3);
 
         $data = [
-            'number' => '4',
-            'street' => 'Nowa',
-            'city' => 'Przyłęki',
-            'zip' => '86-005'
+            'number' => fake()->buildingNumber(),
+            'street' => fake()->streetName(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode()
         ];
 
         $address = $this->actingAs($user)->post('/api/addresses', $data);
