eada41789bea2f89d453e787c0e8e927ac3bf04d
jkryszto00 <jakubkryszto@gmail.com>
2023-01-25 11:09:32 +0100

    Api endpoints addresses auth middleware

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('addresses/search', \App\Http\Controllers\IndexController::class);
-Route::apiResource('addresses', \App\Http\Controllers\AddressController::class);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user', \App\Http\Controllers\UserController::class);
+    Route::apiResource('addresses', \App\Http\Controllers\AddressController::class);
 });
